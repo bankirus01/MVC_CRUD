64705ab6031c17ba90fa26f8d5a7e6c5c8b40054
Andiy Petrik <bankirus01@gmail.com>
2017-12-05 18:04:02 +0200

    add create form in view  - create_form.php

---
+++ view/create_form.php
@@ -0,0 +1,6 @@
+<form action="../controller/create.php" method="post">
+    <input type="text" name="name" placeholder="name" required>
+    <input type="text" name="address" placeholder="address" required>
+    <input type="text" name="salary" placeholder="salary" required>
+    <input type="submit">
+</form>
