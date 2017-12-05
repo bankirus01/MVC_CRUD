a00a378b9813f7b4063362ad30208fd1c822daff
Andiy Petrik <bankirus01@gmail.com>
2017-12-05 18:17:00 +0200

    fix path to create_form.php in view

--- controller/create.php
+++ controller/create.php
@@ -5,4 +5,4 @@ if (isset($_POST["name"]) && isset($_POST["address"]) && isset($_POST["salary"])
     header("Location: edit.php?id=".$id);
 }
 $datetime = date("Y-m-d H:i:s", time());
-require_once __DIR__.'/../view/createForm.php';
+require_once __DIR__.'/../view/create_form.php';
